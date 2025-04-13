2db13c7e3bdda84914b7850e9e5dbf748c7f4b77
pierooLedesma <piero.ledesma@pucp.edu.pe>
2025-04-13 01:47:21 -0500

    Add files via upload

---
+++ Técnicas de Programación/Ciclo 2022-2/Laboratorios/Laboratorio02 H0431 y H0432 - 2022-2/Utils.h
@@ -0,0 +1,27 @@
+
+
+/* 
+ * File:   Utils.h
+ * Author: Alessandro Piero Ledesma Guerra
+ * Código: 20217340
+ * Creado el 29 de marzo de 2025, 11:19 PM
+ */
+
+/* En este "header" llamado "Utils.h", implemento las
+ * las librerías que hay en el sistema. Asímismo, se
+ * definen las constantes a utilizar.
+ */
+
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <fstream>      // Librería del sistema
+#include <iomanip>     // Librería del sistema
+#include <iostream>   // Librería del sistema
+using namespace std;
+#define CANT_MAX_LINEAS 160  // Constante a usar para las líneas del reporte
+#define LONGITUD_DEL_NOMBRE_MEDICO_Y_ESPACIOS 50
+#define LONGITUD_DEL_NOMBRE_PACIENTE_Y_ESPACIOS 49
+
+#endif /* UTILS_H */
+
