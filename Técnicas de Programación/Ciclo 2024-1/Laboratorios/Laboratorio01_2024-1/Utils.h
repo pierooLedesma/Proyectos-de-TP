ac553051e73b90f6bc5284b8a900cc04215a12f6
pierooLedesma <piero.ledesma@pucp.edu.pe>
2025-04-17 23:57:27 -0500

    Add files via upload

---
+++ Técnicas de Programación/Ciclo 2024-1/Laboratorios/Laboratorio01_2024-1/Utils.h
@@ -0,0 +1,24 @@
+
+
+/* 
+ * Archivo:   Utils.h
+ * Autor: Alessandro Piero Ledesma Guerra - 20217340
+ * Creado el 12 de abril de 2025, 08:13 PM
+ */
+
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#define TAM_REPORTE 160
+#define CANT_CARAC_TITULO 22
+#define CANT_CARAC_SUBTITULO 42
+#define NUM_COLUMNAS 8
+#define CANT_CARAC_TITULO_ESTADISTICAS 29
+#define CANT_CARAC_ESTADISTICAS 89
+using namespace std;
+
+#endif /* UTILS_H */
+
