54da335741386dbe281e910a0dc9b6b49a7780b6
pierooLedesma <piero.ledesma@pucp.edu.pe>
2025-06-01 15:15:56 -0500

    Update FuncionesCadenas.cpp

--- Técnicas de Programación/Ciclo 2024-1/Laboratorios/Laboratorio06_2024-1/FuncionesCadenas.cpp
+++ Técnicas de Programación/Ciclo 2024-1/Laboratorios/Laboratorio06_2024-1/FuncionesCadenas.cpp
@@ -45,7 +45,7 @@ void concatenar(char *cadena_1, const char *cadena_2){
     // saber desde dónde empezar a copiar.
     int indice = longitud_cadena(cadena_1);
     
-     // Copiar "cadena_2" al final de "cadena_1" empezando desde el
+    // Copiar "cadena_2" al final de "cadena_1" empezando desde el
     // índice justo después del último carácter de "cadena_1"
     copiar_cadena(&cadena_1[indice], cadena_2);
 }
