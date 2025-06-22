907ea5d22a23991af77f3cc11ee43f7f1675706e
pierooLedesma <piero.ledesma@pucp.edu.pe>
2025-06-22 18:36:35 -0500

    Update Funciones.h

--- Labs. de Programación 2 para el lab. 9 de TP/Laboratorio07_Prog2_a_TP/Funciones.h
+++ Labs. de Programación 2 para el lab. 9 de TP/Laboratorio07_Prog2_a_TP/Funciones.h
@@ -36,25 +36,25 @@ void imprimir_totales_de_la_zona(int cantidad_multas_registradas, double total_m
 void emitir_reporte(const char *nombre_archivo, const struct Zona *arrZonas, int cantidad_zonas);
 void leer_un_evento_de_la_zona(char *&id_leido, char *&placa, int &fecha_infraccion, int &aviso_observado, ifstream &input);
 bool encontrar_drone_velocidad(char *id_leido, const struct DroneDeVelocidad *drones_velocidad,
-                                                          int n_drones_velocidad, int &indice_a_retornar);
+                               int n_drones_velocidad, int &indice_a_retornar);
 bool encontrar_drone_semaforo(char *id_leido, const struct DroneSemaforo *drones_semaforo,
-                                                         int n_drones_semaforo, int &indice_a_retornar);
+                              int n_drones_semaforo, int &indice_a_retornar);
 bool encontrar_drone_estacionamiento(char *id_leido, const struct DroneEstacionamiento *drones_estacionamiento,
-                                                                      int n_drones_estacionamiento, int &indice_a_retornar);
+                                     int n_drones_estacionamiento, int &indice_a_retornar);
 void actualizar_infracciones(struct Infraccion *infracciones, int &cantidad_infracciones, char *placa, 
-                                                 int fecha_infraccion, int aviso_observado, int aviso_permitido,
-                                                 int &cantidad_multas_registradas, double &total_multas_acumuladas);
+                             int fecha_infraccion, int aviso_observado, int aviso_permitido,
+                             int &cantidad_multas_registradas, double &total_multas_acumuladas);
 void procesar_eventos_de_una_zona(struct DroneDeVelocidad *drones_velocidad,
                                 int n_drones_velocidad, struct DroneSemaforo *drones_semaforo, int n_drones_semaforo,
                                 struct DroneEstacionamiento *drones_estacionamiento, int n_drones_estacionamiento,
                                 int &cantidad_multas_registradas, double &total_multas_acumuladas, ifstream &input);
 void procesar_eventos(const char *nombre_carpeta, struct Zona *arrZonas, int cantidad_zonas);
 void leer_datos_de_un_drone(char *&tipo_de_drone, char *&id, char *&ubicacion,
-                                                     int &capacidad_de_almacenamiento, int &aviso, ifstream &input);
+                            int &capacidad_de_almacenamiento, int &aviso, ifstream &input);
 void completar_campo_estructura_drone(struct Drone &un_drone, char *id, char *ubicacion, int capacidad);
 void cargar_datos_de_un_drone(struct DroneDeVelocidad *drones_velocidad, int &n_drones_velocidad,
-                                                         struct DroneSemaforo *drones_semaforo, int &n_drones_semaforo,
-                                                         struct DroneEstacionamiento *drones_estacionamiento, int &n_drones_estacionamiento,ifstream &input);
+                              struct DroneSemaforo *drones_semaforo, int &n_drones_semaforo,
+                              struct DroneEstacionamiento *drones_estacionamiento, int &n_drones_estacionamiento,ifstream &input);
 void procesar_drones(const char *nombre_carpeta, struct Zona *arrZonas, int cantidad_zonas);
 void pasar_todo_a_mayusculas(char *cadena);
 void procesar_zonas(const char *ruta_de_archivo, struct Zona *&arrZonas, int &cantidad_zonas);
